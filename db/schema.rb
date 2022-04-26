2bb3129251621caba70c46af1003df31678a45d7
Alexandra <belliraxia@gmail.com>
2022-04-26 15:21:04 -0400

    adds new model through scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_26_174058) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_26_192015) do
+  create_table "pokemons", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "trading_cards", force: :cascade do |t|
     t.string "name"
     t.string "element"
